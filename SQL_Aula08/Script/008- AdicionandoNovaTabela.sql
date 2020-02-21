d6a26c3cb0199861080d9d40aa9745229f37b1ef
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2020-02-21 09:00:42 -0300

    Adicionando Script - Criando nova tabela com Chave estrangeira

---
+++ SQL_Aula08/Script/008- AdicionandoNovaTabela.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE Carro(
+	id INT IDENTITY (1,1) PRIMARY KEY,
+	Marca VARCHAR(50) NOT NULL,
+	Modelo VARCHAR(50),
+	IdPessoa INT NOT NULL,
+	CONSTRAINT carro_pessoa_fk --Nome da constraint
+		FOREIGN KEY (IdPessoa) --propriedade da tabela
+		REFERENCES Pessoa(Id) -- tabela estrangeira e prop
+);
+
+
+INSERT INTO Carro (Marca, IdPessoa)
+VALUES ('Honda', (SELECT id FROM Pessoa WHERE nome = 'ana'));
+
+SELECT * FROM Carro;
+
+DELETE FROM Carro WHERE Id = (SELECT Id FROM Carro WHERE IdPessoa = 3)
+DELETE FROM PESSOA WHERE id = 2;
